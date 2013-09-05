dcf2a339ea0d6d17c95914fedb795c487ca92af7
Okan Tanrikulu <motanrikulu@gmail.com>
2013-09-04 18:22:45 -0700

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ActiveRecord::Base
-	
+	# before_save { self.email = email.downcase }
+	# has_secure_password
+
+	# An alternative implementation of the above 2 lines
+	has_secure_password
+  	before_save { email.downcase! }
+
+  	validates :password, length: { minimum: 6 }
 	# validates :name, presence: true  is the same as 
 	# validates(:name, presence: true)
-
 	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	# A more complex REGEX that checks for consecutive dots.
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :email, presence: true, 
+						format: { with: VALID_EMAIL_REGEX },
+						uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,17 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(name: "Example User", email: "user@example.com", 
+                            password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to (:password) }
+  it { should respond_to (:password_confirmation) }
+  it { should respond_to (:authenticate) }
 
   it { should be_valid }
 
@@ -29,7 +34,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -44,6 +49,74 @@ describe User do
         @user.email = valid_address
         expect(@user).to be_valid
       end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+  describe "when password is not present" do
+    before { @user = User.new(name: "Example User", email: "user@example.com", 
+                            password: " ", password_confirmation: " ") }
+
+    it { should_not be_valid }
+  end
+
+  # We also want to ensure that the password and confirmation match. 
+  # The case where they do match is already covered by it { should be_valid }, 
+  # so we only need to test the case of a mismatch:
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  # let memoizes its value, so that the first nested describe block below 
+  # invokes let to retrieve the user from the database using find_by, 
+  # but the second describe block doesn’t hit the database a second time
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
     end
   end
 end
